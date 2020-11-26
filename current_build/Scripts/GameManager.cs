79140d0a22d25ec872bacfbebecd7302f3c491df
DTDevelop <46943548+DTDevelop@users.noreply.github.com>
2020-11-26 14:26:01 -0800

    Add files via upload
    debug (general syntax)

--- current_build/Scripts/GameManager.cs
+++ current_build/Scripts/GameManager.cs
@@ -2,17 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// setup gamemanager to test boardmanager previously written
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // create public variable of boardmanager called boardscript
+    public BoardManager boardScript;
+
+    private int level = 3;
+    // testing level 3 as this is where enemies begin to appear
+
+
+    // start is called before the first frame update
+    void Awake()
     {
-        
+      // get&store component reference to board manager script
+      boardScript = GetComponent<BoardManager>();
+      // call init game function
+      InitGame();
+
+    }
+
+    // declare init game
+    void InitGame()
+    {
+      // call setup scene function of boardscript
+      // pass in parameter level, scene takes level specified
+      // can determine num of enemies
+        boardScript.SetupScene(level);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
