beafb2b4a28b41f93b2ebec0c52d07472899a0d0
DTDevelop <46943548+DTDevelop@users.noreply.github.com>
2020-11-26 15:59:52 -0800

    Add files via upload

---
+++ current_build/Scripts/Loader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// check if game has been instantiated
+// if not, instantiate one from prefab
+
+public class Loader : MonoBehaviour
+{
+
+    public GameObject gameManager;
+
+    void Awake()
+    {
+        // using public variable created in gamemanager script
+        // accessing from this loader script
+    if (GameManager.instance == null)
+            Instantiate(gameManager);
+    }
+}
