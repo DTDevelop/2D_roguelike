18c029156a9ca4f90247aca478b78a5d88d6de16
DTDevelop <46943548+DTDevelop@users.noreply.github.com>
2020-11-27 22:09:20 -0800

    Add files via upload

--- current_build/Scripts/GameManager.cs
+++ current_build/Scripts/GameManager.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 // setup gamemanager to test boardmanager previously written
 public class GameManager : MonoBehaviour
 {
+    // public -- accessible outside of class
+    // static -- variable belongs to class itself, opposed to instance of class
+    // can access public functions/variables of gamemanager from any script in game
+    public static GameManager instance = null;
     // create public variable of boardmanager called boardscript
     public BoardManager boardScript;
+    public int playerFoodPoints = 100;
+    [HideInInspector] public bool playersTurn = true;
+    // hide, although public, won't be displayed in editor
 
     private int level = 3;
     // testing level 3 as this is where enemies begin to appear
@@ -15,10 +22,19 @@ public class GameManager : MonoBehaviour
     // start is called before the first frame update
     void Awake()
     {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+            // avoids having 2 instance of game manager
+
+        DontDestroyOnLoad(gameObject);
+        // when loading new scene, normally all objects in hierarchy destroyed
+        // used to keep track of score between scenes, allows to persist between scenes
       // get&store component reference to board manager script
-      boardScript = GetComponent<BoardManager>();
+        boardScript = GetComponent<BoardManager>();
       // call init game function
-      InitGame();
+        InitGame();
 
     }
 
@@ -29,6 +45,13 @@ public class GameManager : MonoBehaviour
       // pass in parameter level, scene takes level specified
       // can determine num of enemies
         boardScript.SetupScene(level);
+    }
+
+    public void GameOver()
+    {
+        // disables gameManager
+        enabled = false;
+
     }
 
     // Update is called once per frame
